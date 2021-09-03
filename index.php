89ff467b9939aefc33b607ab90efbe2c28d7cd76
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-03 11:41:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <html>
-<head>
-<title>Loop variabele</title>
-</head>
-<body>
-<?php
-$tekst = "sannie";
-for ($i=1, 3, 5;)
-echo "<strong>$tekst</strong>";
-for ($i=2, 4;)
-echo $tekst
+    <head>
+        <title>Opdr F</title>
+    </head>
+    <body>
+        <?php
 
-?>
-</body>
+        $tekst = "sannie";
+        for ($i=1, 3, 5;)
+            echo "<strong>$tekst</strong>";
+        for ($i=2, 4;)
+             echo $tekst
+
+        ?>
+    </body>
 </html>
