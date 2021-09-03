aadccd8ea75a4ae5d1dd9ef5172b828ba119ab12
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-03 11:45:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
         $tekst = "sannie";
         for ($i=1, 3, 5;)
             echo "<strong>$tekst</strong>";
-        for ($i=2, 4;)
-             echo $tekst;
+ //       for ($i=2, 4;)
+//        echo $tekst;
 
         ?>
     </body>
