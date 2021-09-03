4c27813c7435badd936a90562bceb50e926a50b2
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-03 11:47:28 +0200

    itjes fixen

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <?php
 
         $tekst = "sannie";
-        for ($i=1, 3, 5;)
+        for ($i=1, $i=3, $i=5;)
             echo "<strong>$tekst</strong>";
  //       for ($i=2, 4;)
 //        echo $tekst;
