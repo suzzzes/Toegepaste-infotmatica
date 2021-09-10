14cdfb60dda3124c887f898b9fb4ce32c557140a
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-10 10:31:30 +0200

    Update opdr3.php

---
+++ opdr3.php
@@ -0,0 +1,17 @@
+<?php
+if (array_key_exists('tekst',$_REQUEST))
+	$tekst=$_REQUEST['tekst'];
+else
+	$tekst="Zend";
+?>
+<html>
+<head>
+<title>Type conversie</title>
+</head>
+<body bgcolor="#FFFFFF">
+<form method="post" >
+    <p><input type="text" size="20" name="tekst" value="<?php echo $tekst; ?>"><br>
+    <input type="submit" name="start" value="zend"></p>
+</form>
+</body>
+</html>
