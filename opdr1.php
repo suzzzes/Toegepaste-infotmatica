7fe2b104cbe80147106c7c0a8761f87746ce539d
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-10 08:51:10 +0200

    Create opdr1.php

---
+++ opdr1.php
@@ -0,0 +1,21 @@
+opdr1
+
+<html>
+    <head>
+        <title>loop variable</title>
+    </head>
+    <body>
+        <?php
+
+        $tekst = "Sannie";
+        for ($i=0; $i<8; $i++)
+        if ($i %2 == 0)             //als je i deelt door twee is hij niet dik gedrukt.
+        
+            echo " <font size= $i> $tekst </font>";
+        else
+
+            echo " <strong> <font size= $i> $tekst </font> </strong>";
+           
+        ?>
+    </body>
+</html>
