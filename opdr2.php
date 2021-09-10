ffb1c5a67b1590dabe29c3444d9e7b07c7f2c93c
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-10 09:47:34 +0200

    Update opdr2.php

--- opdr2.php
+++ opdr2.php
@@ -7,8 +7,8 @@
 
     setlocale(LC_ALL, 'nl_NL' ); //zet de data in het nederlands
     
-    echo "Het is vandaag: <br>";
-    echo strftime("%A %e %B %Y <br>");
+    echo "Het is vandaag: <br>";    //br zorgt voor nieuwe regel
+    echo strftime("%A %e %B %Y <br>");  // strftime zorgt ervoor dat hij in het nederlands gaat ipv date
     echo "De tijd is ";
     echo date('H:i');
   ?>
