78cae2b57113f605d3f18572b5fc69d8a44fc267
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-11 19:46:23 +0200

    Update opdr3a.php

--- opdr3a.php
+++ opdr3a.php
@@ -1,9 +1,10 @@
 <?php
+
+$button="Zend";
 if (array_key_exists('tekst',$_REQUEST))
 	$tekst=$_REQUEST['tekst'];
 else
 	$tekst="";
-    echo preg_replace('/[0-9]/', '', $tekst);
 
 ?>
 <html>
@@ -12,8 +13,13 @@ else
 </head>
 <body bgcolor="#FFFFFF">
 <form method="post" >
-    <p><input type="text" size="20" name="tekst" value="<?php echo $tekst; ?>"><br>
-    <input type="submit" name="start" value="zend"></p>
+    <p><input type="text" size="20" name="tekst" value="<?php echo preg_replace('/[a-zA-Z]/', '', $tekst); ?>"><br>
+    <input type="submit" name="start" value="<?php if(!empty($tekst))
+    		echo preg_replace('/[0-9]/', '', $tekst); 
+    		else
+    	   echo $button
+    	
+    ?>"></p>
 </form>
 </body>
 </html>
