dd8bb32211ffd8180b81671a77f4b59b7b9349b5
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-11 20:02:25 +0200

    extra opdr

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
         <h1> Sannes informatica opdrachten </h1><hr>
         <a href = "/opdr1.php"> Opdracht 1 </a><BR>
         <a href = "/opdr2.php"> opdracht 2 </a><BR> 
-        <a href = "/opdr3a.php"> opdracht 3 </a><BR> 
+        <a href = "/opdr3a.php"> opdracht 3b </a><BR> 
+        <a href = "/opdr3c.php"> opdracht 3c </a><BR> 
 
            
         
--- opdr3a.php
+++ opdr3a.php
@@ -1,7 +1,7 @@
 <?php
 
 $button="Zend";
-if (array_key_exists('tekst',$_REQUEST))
+if (array_key_exists('tekst',$_REQUEST)) // kijkt of er tekst staat in de variable
 	$tekst=$_REQUEST['tekst'];
 else
 	$tekst="";
@@ -13,7 +13,7 @@ else
 </head>
 <body bgcolor="#FFFFFF">
 <form method="post" >
-    <p><input type="text" size="20" name="tekst" value="<?php echo preg_replace('/[a-zA-Z]/', '', $tekst); ?>"><br>
+    <p><input type="text" size="20" name="tekst" value="<?php echo preg_replace('/[a-zA-Z]/', '', $tekst); ?>"><br> 
     <input type="submit" name="start" value="<?php if(!empty($tekst))
     		echo preg_replace('/[0-9]/', '', $tekst); 
     		else
@@ -23,3 +23,9 @@ else
 </form>
 </body>
 </html>
+
+<?php
+    // preg_replace filtert de letters uit de tekst en laat alleen de cijfers over in regel 17
+    // in regel 19 kijkt de if statement of het tekstvak leeg is, is deze dat niet dan zet hij hier de ingevoede letters in.
+    // is hij dat wel, dan zet hij de variable butten erin
+    ?>
