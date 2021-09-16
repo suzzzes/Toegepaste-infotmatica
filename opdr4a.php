5f9cbfc012a8a7afdb33789605e69ddc8cfd2d5a
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-16 15:46:39 +0200

    Update opdr4a.php

--- opdr4a.php
+++ opdr4a.php
@@ -16,11 +16,11 @@ foreach($alphabet as $character){
 }
 
 
-echo "De tekst is :".$letter."<br>";
-echo "De lengte van de tekst ".$letter." is :".strlen($letter)." karakters.<br>";
-echo "De tekst uvw staat op positie ".strpos($letter,"uvw")."<br>";
-echo "In de tekst uvw wordt vervangen door UVW ".str_replace("uvw","UVW",$letter)."<br>";
-echo "Uit de tekst wordt een deel genomen van positie 5, 10 karakters ->  ".substr($letter,5,10)."<br>";
+echo "De tekst is :".$character."<br>";
+echo "De lengte van de tekst ".$character." is :".strlen($character)." karakters.<br>";
+echo "De tekst uvw staat op positie ".strpos($character,"uvw")."<br>";
+echo "In de tekst uvw wordt vervangen door UVW ".str_replace("uvw","UVW",$character)."<br>";
+echo "Uit de tekst wordt een deel genomen van positie 5, 10 karakters ->  ".substr($character,5,10)."<br>";
 echo "De karkaters van ascii 48 t/m 57 is : ";
 for ($x=48;$x<58;$x++)
    echo chr($x);
