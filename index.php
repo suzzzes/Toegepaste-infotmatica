4b35a7f9802151d55af8dca18eefdaea0dd049cf
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-16 15:26:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <a href = "/opdr2.php"> opdracht 2 </a><BR> 
         <a href = "/opdr3a.php"> opdracht 3b </a><BR> 
         <a href = "/opdr3c.php"> opdracht 3c </a><BR> 
-
+        <a href = "/opdr4a.php"> opdracht 3c </a><BR> 
            
         
     </body>
