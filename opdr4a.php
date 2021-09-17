180b15b80499cefca96ae5c37a3ab8775d5c482e
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-17 10:45:21 +0200

    Update opdr4a.php

--- opdr4a.php
+++ opdr4a.php
@@ -3,26 +3,15 @@
 <title>Strings</title>
 </head>
 <body bgcolor="#FFFFFF">
-
 <?php
-
-// generate array with the characters from the alphabet 
-// A to Z
-$alphabet = range("a", "z");
-        
-// Iterate over every character and create a directory with the name of the current character
-foreach($alphabet as $character){
-    mkdir("./" . $character);
-}
-
-
-echo "De tekst is :".$character."<br>";
-echo "De lengte van de tekst ".$character." is :".strlen($character)." karakters.<br>";
-echo "De tekst uvw staat op positie ".strpos($character,"uvw")."<br>";
-echo "In de tekst uvw wordt vervangen door UVW ".str_replace("uvw","UVW",$character)."<br>";
-echo "Uit de tekst wordt een deel genomen van positie 5, 10 karakters ->  ".substr($character,5,10)."<br>";
+$tekst="abcdefghijklmnopqrstuvwxyz";
+echo "De tekst is :".$tekst."<br>";
+echo "De lengte van de tekst ".$tekst." is :".strlen($tekst)." karakters.<br>";
+echo "De tekst uvw staat op positie ".strpos($tekst,"uvw")."<br>";
+echo "In de tekst uvw wordt vervangen door UVW ".str_replace("uvw","UVW",$tekst)."<br>";
+echo "Uit de tekst wordt een deel genomen van positie 5, 10 karakters ->  ".substr($tekst,5,10)."<br>";
 echo "De karkaters van ascii 48 t/m 57 is : ";
-for ($x=48;$x<58;$x++)
+for ($x=97;$x<122;$x++)
    echo chr($x);
 echo "<br>";
 ?>
