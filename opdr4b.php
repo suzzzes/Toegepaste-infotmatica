98983d967b5eba5479e5606f82ae5dc7cdca7d68
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-17 11:19:11 +0200

    Update opdr4b.php

--- opdr4b.php
+++ opdr4b.php
@@ -50,18 +50,18 @@ if ($sep2=="") $sep2=".";
 if (!isset($tekst1)) $tekst1=$_POST['tekst'];
 if (strlen($tekst1)>0){
   echo "<table border=3><tr>"."\n";
-  $tabel3= explode ($sep3,$tekst1);
+  $tabel3= explode ($sep1,$tekst1);
   for ($y=0;$y<count($tabel3);$y++){
-    $tabel4=explode($sep3,$tabel4[$y]);
+    $tabel4=explode($sep1,$tabel4[$y]);
       for ($x=0;$x<count($tabel4);$x++)
           echo "<td>".$tabel4[$x]."</td>";
       echo "</tr><tr>";
      }
-     echo "</table>";
+     echo "</table1>";
 }
 if ($tekst1=="") $tekst1="abc,def,789.abc,123,456";
-if ($sep3=="") $sep3=",";
-if ($sep4=="") $sep4=".";
+if ($sep1=="") $sep1=",";
+if ($sep2=="") $sep2=".";
 ?>
 
 <form method="post" >
@@ -69,8 +69,6 @@ if ($sep4=="") $sep4=".";
 <input type="text" size="1" name="sep1" value="<?php echo $sep1; ?>"><br>
 <input type="text" size="1" name="sep2" value="<?php echo $sep2; ?>"><br>
 <input type="text" size="50" name="tekst" value="<?php echo $tekst1; ?>"><br>
-<input type="text" size="1" name="sep1" value="<?php echo $sep3; ?>"><br>
-<input type="text" size="1" name="sep2" value="<?php echo $sep4; ?>"><br>
 <input type="submit" name="start" value="zend"></p>
 </form>
 </body>
