bb3f9ddce07345f39b371dc8169a7f3e8b824cd3
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-17 11:23:49 +0200

    Update opdr4b.php

--- opdr4b.php
+++ opdr4b.php
@@ -46,6 +46,7 @@ if (strlen($tekst)>0){
 if ($tekst=="") $tekst="123,456,789.abc,def,ghi";
 if ($sep1=="") $sep1=",";
 if ($sep2=="") $sep2=".";
+if ($sep3="") $sep3=":";
 
 if (!isset($tekst1)) $tekst1=$_POST['tekst1'];
 if (strlen($tekst1)>0){
