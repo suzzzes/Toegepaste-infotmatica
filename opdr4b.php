21216f5610867d77cdf6c29c3eb83c4bde3ce9e8
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-17 10:53:52 +0200

    Update opdr4b.php

---
+++ opdr4b.php
@@ -0,0 +1,45 @@
+<?php
+if (array_key_exists('sep1',$_REQUEST))
+	$sep1=$_REQUEST['sep1'];
+else
+	$sep1=",";
+if (array_key_exists('sep2',$_REQUEST))
+	$sep2=$_REQUEST['sep2'];
+else
+	$sep2=".";
+if (array_key_exists('tekst',$_REQUEST))
+	$tekst=$_REQUEST['tekst'];
+else
+	$tekst="";
+?>
+<html>
+<head>
+<title>arrays</title>
+</head>
+<body bgcolor="#FFFFFF">
+<?php
+if (!isset($tekst)) $tekst=$_POST['tekst'];
+if (strlen($tekst)>0){
+  echo "<table border=3><tr>"."\n";
+  $tabel1= explode ($sep2,$tekst);
+  for ($y=0;$y<count($tabel1);$y++){
+    $tabel2=explode($sep1,$tabel1[$y]);
+      for ($x=0;$x<count($tabel2);$x++)
+          echo "<td>".$tabel2[$x]."</td>";
+      echo "</tr><tr>";
+     }
+     echo "</table>";
+}
+if ($tekst=="") $tekst="123,456,789.abc,def,ghi";
+if ($sep1=="") $sep1=",";
+if ($sep2=="") $sep2=".";
+?>
+
+<form method="post" >
+<p><input type="text" size="50" name="tekst" value="<?php echo $tekst; ?>"><br>
+<input type="text" size="1" name="sep1" value="<?php echo $sep1; ?>"><br>
+<input type="text" size="1" name="sep2" value="<?php echo $sep2; ?>"><br>
+    <input type="submit" name="start" value="zend"></p>
+</form>
+</body>
+</html>
