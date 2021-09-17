2055e0ba4c3c47073761274ebe77c52e9ed9046e
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-09-17 10:46:19 +0200

    Update opdr4a.php

--- opdr4a.php
+++ opdr4a.php
@@ -10,7 +10,7 @@ echo "De lengte van de tekst ".$tekst." is :".strlen($tekst)." karakters.<br>";
 echo "De tekst uvw staat op positie ".strpos($tekst,"uvw")."<br>";
 echo "In de tekst uvw wordt vervangen door UVW ".str_replace("uvw","UVW",$tekst)."<br>";
 echo "Uit de tekst wordt een deel genomen van positie 5, 10 karakters ->  ".substr($tekst,5,10)."<br>";
-echo "De karkaters van ascii 48 t/m 57 is : ";
+echo "De karkaters van ascii 97 t/m 122 is : ";
 for ($x=97;$x<122;$x++)
    echo chr($x);
 echo "<br>";
