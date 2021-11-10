c2cb48303869a6e7c916f6b59bbac4a35465db7f
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-11-10 14:52:45 +0100

    iets

--- opdr 5/opdr5.php
+++ opdr 5/opdr5.php
@@ -5,7 +5,6 @@
 
   <body>
     <?php
-      function int_2_gif($c){
         settype($c,"integer");
         settype($c,"string");
 
@@ -18,19 +17,19 @@
         }
       }
   
-      date_default_timezone_set('Europe/Amsterdam');  //Stelt de tijdzone van Nederland in.
-      setlocale(LC_ALL, 'nl_NL');                     //Verandert de taal naar het Nederlands.
+      setlocale(LC_ALL, 'nl_NL');                     //Deze functie verandert de taal naar het Nederlands.
+      date_default_timezone_set('Europe/Amsterdam');  //Deze functie stelt de tijdzone van Nederland in.
 
       $today = getdate();
 
-      //Print de datum
+      //De datum printen
       echo "Het is vandaag: </br>";     //Print alleen tekst
       echo strftime("%A");              //Print de dag van de week
         int_2_gif($today["mday"]);      //Converteer dag in cijfers
       echo strftime("%B");              //Print de maand
         int_2_gif($today["year"]);      //Converteer jaar in cijfers
 
-      //Print de tijd  
+      //De tijd printen 
       echo "</br>De tijd is:</br>";     //Print alleen tekst
         int_2_gif($today["hours"]);     //Converteer het uur
       echo ":";                         //Print :
