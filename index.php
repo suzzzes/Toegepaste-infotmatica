29c3785734046be22ebc17ecb164a7f62fc3af71
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-11-10 11:49:13 +0100

    opdr4c

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <a href = "/opdr3c.php"> opdracht 3c </a><BR> 
         <a href = "/opdr4a.php"> opdracht 4a </a><BR> 
         <a href = "/opdr4b.php"> opdracht 4b </a><BR>  
+        <a href = "/opdr4c.php"> opdracht 4c </a><BR>  
         
     </body>
 </html>
---
+++ opdr4c.php
@@ -0,0 +1,38 @@
+<?php
+  if (array_key_exists('email',$_REQUEST))            //Deze functie haalt de gegevens uit de tekstbox
+	$email=$_REQUEST['email'];
+
+  if (filter_var($email, FILTER_VALIDATE_EMAIL)) {    //Deze functie controleert of het email adres geldig is
+    echo "Geldige email <br>";
+  }
+  else{
+    echo "Niet geldige email <br>";
+  }
+  
+  if (array_key_exists('internet',$_REQUEST))         //Deze functie haalt de gegevens uit de tekstbox
+	$internet=$_REQUEST['internet'];
+  
+  if (filter_var($internet, FILTER_VALIDATE_URL)) {   //Deze functie controleert of de url wel geldig is
+    echo "Geldige URL <br>";
+  }
+  else{
+    echo "Niet geldige URL <br>";
+  }
+
+?>
+
+<html>
+  <head>
+    <title>Opdracht 4c</title>
+  </head>
+  <body>
+
+    <form>
+      <input type="text" name="email" placeholder="e-mailadres" value="<?php echo $email?>">
+      <input type="text" name="internet" placeholder="Webadres" value="<?php echo $internet?>">
+      <input type="submit"value ="zend">
+    </form>
+
+  </body>
+</html>
+
