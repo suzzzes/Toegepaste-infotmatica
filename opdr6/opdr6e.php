b28aff81f3e3ed745fde8fac8ce70eab5e6e1404
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-11-24 11:28:58 +0100

    Update opdr6e.php

--- opdr6/opdr6e.php
+++ opdr6/opdr6e.php
@@ -1,44 +1,4 @@
 <?php
-if (isset($_REQUEST['naam0']))
-	$naam0=$_REQUEST["naam0"];
-else
-	$naam0="jan";
-if (isset($_REQUEST['adres0']))
-	$adres0=$_REQUEST["adres0"];
-else
-	$adres0="";
-
-$gev = 0;
-$fd = fopen("DATA/namen.txt","r");
-while (!feof($fd) && $gev == 0) {
-	$buffer = trim(fgets($fd, 4096));
-	$namen = explode(",", $buffer);
-	for ($y = 0; $y < count($namen); $y++) {
-		if ($naam0 == $namen[$y]) {
-			$gev = 1;
-			break;
-		}
-	}
-}
-fclose($fd);
-
-$gev2 = 0;
-if ($gev == 1) {
-	$fd2 = fopen("DATA/adressen.txt","r");
-	while (!feof($fd2) && $gev2 == 0) {
-		$adrbuffer = trim(fgets($fd2, 4096));
-		$adressen = explode(",", $adrbuffer);
-		for ($x = 0; $x < count($adressen); $x++) {
-			$adres = explode(".", $adressen[$x]);
-			if ($naam0 == $adres[0]) {
-				$adres0 = str_replace($naam0.".", "", $adressen[$x]);
-				$gev2 = 1;
-				break;
-			}
-		}
-	}
-	fclose($fd2);
-}
 
 if (getenv('HTTP_X_FORWARDED_FOR')){ 
   $ip = getenv('HTTP_X_FORWARDED_FOR'); 
