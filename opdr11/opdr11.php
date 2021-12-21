5109cfa40fcbae714a982309c68b9b6809675faf
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-12-21 14:58:41 +0100

    Update opdr11.php

--- opdr11/opdr11.php
+++ opdr11/opdr11.php
@@ -1,20 +1,33 @@
+<html> 
+	<head> 
+		<title>Opdracht 10</title>
+		<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script type = "text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+		<script type = "text/javascript">
+			google.charts.load ('current', {'packages':['corechart']}); // laat libarys in
+			google.charts.setOnLoadCallback (loadData); // wacht tot geladen is, voordat de data erin gezet wordt
 
-<html>
-    <head>
-        <title>loop variable</title>
-    </head>
-    <body>
-        <?php
+	</head> 
+	<body>
+		
+		<?php
+		
+		      if(isset($_POST['button1'])) {
+				echo "This is Button1 that is selected";
+			}
+			if(isset($_POST['button2'])) {
+				echo "This is Button2 that is selected";
+			}
+		?>
+		  
+		<form method="post">
+			<input type="submit" name="button1"
+					value="Button1"/>
+			  
+			<input type="submit" name="button2"
+					value="Button2"/>
+		</form>
+		?>
 
-        $tekst = "Sannie";
-        for ($i=0; $i<8; $i++)
-        if ($i %2 == 0)             //als je i deelt door twee is hij niet dik gedrukt.
-        
-            echo " <font size= $i> $tekst </font>";
-        else
-
-            echo " <strong> <font size= $i> $tekst </font> </strong>";
-           
-        ?>
-    </body>
+	</body> 
 </html>
