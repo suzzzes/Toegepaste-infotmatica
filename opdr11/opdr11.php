afc1d08552424187d2228abe0ed2536cf9c82544
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-12-21 14:32:41 +0100

    Update opdr11.php

--- opdr11/opdr11.php
+++ opdr11/opdr11.php
@@ -1,23 +1,3 @@
-<html> 
-	<head> 
-		<title>Opdracht 11</title>
-        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script type = "text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-		<script type = "text/javascript">
-			google.charts.load ('current', {'packages':['corechart']}); // laat libarys in
-			google.charts.setOnLoadCallback (loadData); // wacht tot geladen is, voordat de data erin gezet wordt
-
-
-
-        <form method= "post">
-        ipadres: <input type="submit" size="5" name="Lamp" value="<?php echo $Lamp;?> "> 
-        
-
-	</head> 
-	<body>
-	<form method= "post">
-        ipadres: <input type="submit" size="5" name="Lamp" value="<?php echo $Lamp;?> "> 
-
     <?php
     $Lamp = "iets";
 
@@ -27,4 +7,18 @@
 
 
     ?>
+	<html> 
+	<head> 
+		<title>Opdracht 11</title>
+	</head> 
+
+        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script type = "text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+		<script type = "text/javascript">
+			google.charts.load ('current', {'packages':['corechart']}); // laat libarys in
+			google.charts.setOnLoadCallback (loadData); // wacht tot geladen is, voordat de data erin gezet wordt
+        
+	<body>
+	<form method= "post">
+        ipadres: <input type="submit" size="5" name="Lamp" value="<?php echo $Lamp;?> "> 
 	</html>
