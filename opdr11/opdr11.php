649a97cdfb497488c7f1eeba613d603b8351d758
suzzzes <54936045+suzzzes@users.noreply.github.com>
2021-12-21 14:52:40 +0100

    Update opdr11.php

--- opdr11/opdr11.php
+++ opdr11/opdr11.php
@@ -1,31 +1,20 @@
-<html> 
-	<head> 
-		<title>Opdracht 11</title>
-	</head> 
 
-        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script type = "text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-		<script type = "text/javascript">
-			google.charts.load ('current', {'packages':['corechart']}); // laat libarys in
-			google.charts.setOnLoadCallback (loadData); // wacht tot geladen is, voordat de data erin gezet wordt
+<html>
+    <head>
+        <title>loop variable</title>
+    </head>
+    <body>
+        <?php
+
+        $tekst = "Sannie";
+        for ($i=0; $i<8; $i++)
+        if ($i %2 == 0)             //als je i deelt door twee is hij niet dik gedrukt.
         
-	<body>
+            echo " <font size= $i> $tekst </font>";
+        else
 
-	<?php
-	echo "poep tot de max";
-	echo "Vul X in";
-		$Lamp = "iets";
-
-		if ($_SERVER['REQUEST_METHOD'] == "POST") {
-			// actie
-		}
-
-
-		?>
-
-		<form method= "post">
-		<input type="submit" size="5" name="Lamp" value="iets"/> 
-		</form>
-
-
-	</html>
+            echo " <strong> <font size= $i> $tekst </font> </strong>";
+           
+        ?>
+    </body>
+</html>
